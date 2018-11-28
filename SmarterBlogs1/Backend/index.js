9e025abfcdb2aab5794c3671f5da7e8c79328670
Saumya Goyal <saumya.iis@gmail.com>
2018-11-28 00:47:13 -0800

    modified node code

--- SmarterBlogs1/Backend/index.js
+++ SmarterBlogs1/Backend/index.js
@@ -11,7 +11,7 @@ var cors = require("cors");
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 //set up session variable
-
+let data1 ='';
 app.use(
   session({
     secret: "linkedin",
@@ -47,6 +47,36 @@ app.post("/search", (req, res) => {
   console.log("in search people");
   console.log("search for:", req.body);
   search.search(req,res);
+});
+
+app.post('/autocorrect', function(req,res) {
+  console.log("body:",req.body.userInput);
+  var request = req.body.userInput;
+  request = request.split(' ').join('+');
+  console.log("request:",request);
+  var request1= request.slice(0,-1);
+  console.log("request:",request1);
+  const http = require('http');
+  http.get('http://18.188.195.95:5000/autoComplete/?phrase='+request1, (resp) => {
+  let data = '';
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(data);
+    res.send(data)
+  });
+  data1 = data; 
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+  //data1 = {"suggestions": ["Republicans", "to trump\u2014boy bye", "trumpster charged with", "religious trumpians suffer", "trumpians suffer from", "could be trumped", "he stop trumpism?", "exactly is trumpism?", "version of trumpism?", "trumpism: \u2018it\u2019s the"]} ;
+  //res.send(data1);
+//res.send(data1);
+
 });
 
 
--- SmarterBlogs1/Backend/match_phrase.js
+++ SmarterBlogs1/Backend/match_phrase.js
@@ -1,10 +1,13 @@
 exports.search = function (req,res) {
   console.log("Query:" ,req.body)
     'use strict';
+    var query1 = req.body.query;
+    var stringArray = query1.split(" ");
+    query1 = stringArray[0];
   
     const elasticsearch = require('elasticsearch');
     const esClient = new elasticsearch.Client({
-      host: '127.0.0.1:9200',
+      host: 'https://elastic:nbqpHxH5ErbXdkBNJK5YjbVt@1a909042abb2466fbd45863e6a6a6ee3.us-east-1.aws.found.io:9243',
       log: 'error'
     });
   
@@ -20,21 +23,21 @@ exports.search = function (req,res) {
         from: 0,
         query: {
           match: {
-            Heading: {
-              query: req.body.query,
+            title: {
+              query: query1,
               operator: 'and'
             }
           }
         }
       };
   
-      console.log(`retrieving documents whose Heading matches phrase '${body.query.match.Heading.query}' (displaying ${body.size} items at a time)...`);
-      search('library', body)
+      console.log(`retrieving documents whose Heading matches phrase '${body.query.match.title.query}' (displaying ${body.size} items at a time)...`);
+      search('app', body)
       .then(results => {
         res.send(results);
         console.log(`found ${results.hits.total} items in ${results.took}ms`);
         if (results.hits.total > 0) console.log(`returned Heading Headings:`);
-        results.hits.hits.forEach((hit, index) => console.log(`\t${body.from + ++index} - ${hit._source.Heading} (score: ${hit._score})`));
+        results.hits.hits.forEach((hit, index) => console.log(`\t${body.from + ++index} - ${hit._source.title} - ${hit._id} (score: ${hit._score})`));
       })
       .catch(console.error);
     };
